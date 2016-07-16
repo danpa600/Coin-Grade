de4e8f10acefd14b43cd09840357657631e4083f
dan.pautler@gmail.com <dan.pautler@gmail.com>
2016-07-15 22:03:20 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/coingrade/MainCoinGradeActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.coingrade;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class MainCoinGradeActivity extends AppCompatActivity {
+
+    private static String VALUE = "myVaule";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_coin_grade);
+    }
+}
